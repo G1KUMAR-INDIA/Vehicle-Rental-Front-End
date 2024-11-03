e69a19fac85b0bbdd96e02bfecf1efa16a7b3006
R JEEVAN KUMAR <g1kumar215@gmail.com>
2024-11-03 20:59:36 +0530

    FrontEnd

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='bg-slate-100 duration-3000 -z-20'>
+    <div className='bg-slate-100 duration-300 -z-20'>
       <div className="container min-h-[620px] flex">
         <div className='grid place-items-center grid-cols-1 sm:grid-cols-2'>
-            <div data-aos="zoom" data-aos-duration="1500" className='order-1 sm:order-2'>
+            <div data-aos="zoom" data-aos-duration="150" className='order-1 sm:order-2'>
                 <img src="https://shorturl.at/236JN" alt="Vehicles" className='relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]' />
             </div>
             <div className='order-2 sm:order-1 space-y-5 sm:pr-32'>
